8826c4b77fe0d6c029fecda405428652e73152d7
Lebed-kun <lebed-kun@mail.ru>
2019-01-27 14:30:04 +0400

    Refactor phase 2

--- src/utils/choose_color/choose_color_helper.js
+++ src/utils/choose_color/choose_color_helper.js
@@ -1,6 +1,35 @@
 import Draw from '../draw';
 import Convert from '../convert_units';
 
+function setHslProperties(options, props, movePen) {
+  // Set up options
+  const contextPen = options.contextPen;
+  const canvas = options.canvas;
+  const component = options.component;
+  const colorPickerOptions = options.colorPickerOptions;
+
+  const clickCoordinates = Draw.getMousePos(canvas, colorPickerOptions.event);
+
+  // Getting hue and saturation values
+  const data = canvas.getContext('2d').getImageData(clickCoordinates.x, clickCoordinates.y, 1, 1).data;
+  const dataHsl = Convert.rgbToHsl(data[0], data[1], data[2]);
+  const hsl = {
+    hue : props['hue'] ? dataHsl.hue : component.state.hue,
+    saturation : props['saturation'] ? dataHsl.saturation : component.state.saturation,
+    lightness : props['lightness'] ? dataHsl.lightness : component.state.lightness
+  }
+
+  const showColor = Convert.formatColor(hsl.hue, hsl.saturation, hsl.lightness);
+
+  component.setState(hsl);
+
+  // Sending color value
+  component.props.onSelect(showColor);
+
+  // Moving pointer
+  movePen(contextPen, canvas, clickCoordinates);
+};
+
 const ChooseColorHelper = {
   drawHuePen : (huePenClass, hueCircleSelector) => {
     Draw.drawPen(`.${huePenClass}`, hueCircleSelector, (contextPen, canvas) => {
@@ -24,54 +53,25 @@ const ChooseColorHelper = {
   },
 
   setHueSaturation : options => {
-    // Set up options
-    const contextPen = options.contextPen;
-    const canvas = options.canvas;
-    const component = options.component;
-    const colorPickerOptions = options.colorPickerOptions;
-
-    // Moving hue-staruration pointer
-    contextPen.clearRect(0, 0, canvas.width, canvas.height);
-    const clickCoordinates = Draw.getMousePos(canvas, colorPickerOptions.event);
-    contextPen.beginPath();
-    contextPen.arc(clickCoordinates.x, clickCoordinates.y, 2, 0, 2 * Math.PI);
-    contextPen.stroke();
-    contextPen.closePath();
-
-    // Getting hue and saturation values
-    const data = canvas.getContext('2d').getImageData(clickCoordinates.x, clickCoordinates.y, 1, 1).data;
-    const hueSaturation = Convert.rgbToHsl(data[0], data[1], data[2]);
-
-    const showColor = Convert.formatColor(hueSaturation.hue, hueSaturation.saturation, component.state.lightness);
-
-    component.setState( { hue : hueSaturation.hue, saturation : hueSaturation.saturation});
-
-    // Sending color value
-    component.props.onSelect(showColor);
+    setHslProperties(options, {
+      'hue' : true,
+      'saturation' : true
+    }, (contextPen, canvas, clickCoordinates) => {
+      // Moving hue-staruration pointer
+      contextPen.clearRect(0, 0, canvas.width, canvas.height);
+      contextPen.beginPath();
+      contextPen.arc(clickCoordinates.x, clickCoordinates.y, 2, 0, 2 * Math.PI);
+      contextPen.stroke();
+      contextPen.closePath();
+    });
   },
 
   setLightness : options => {
-    // Set up options
-    const contextPen = options.contextPen;
-    const canvas = options.canvas;
-    const component = options.component;
-    const colorPickerOptions = options.colorPickerOptions;
-
-    // Moving lightness pointer
-    contextPen.clearRect(0, 0, canvas.width, canvas.height);
-    const clickCoordinates = Draw.getMousePos(canvas, colorPickerOptions.event);
-    contextPen.strokeRect(0, clickCoordinates.y, canvas.width, 3);
-
-    // Getting lightness value
-    const data = canvas.getContext('2d').getImageData(clickCoordinates.x, clickCoordinates.y, 1, 1).data;
-    const lightness = Convert.rgbToHsl(data[0], data[1], data[2]).lightness;
-
-    const showColor = Convert.formatColor(component.state.hue, component.state.saturation, lightness);
-
-    component.setState({ lightness : lightness });
-
-    // Sending color value
-    component.props.onSelect(showColor)
+    setHslProperties(options, { 'lightness' : true }, (contextPen, canvas, clickCoordinates) => {
+      // Moving lightness pointer
+      contextPen.clearRect(0, 0, canvas.width, canvas.height);
+      contextPen.strokeRect(0, clickCoordinates.y, canvas.width, 3);
+    });
   }
 }
 
